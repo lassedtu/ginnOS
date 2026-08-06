71d4d5bdf0c0a84c11a836bcb9deb1294a2b2934
lassedtu <s255896@student.dtu.dk>
2026-08-06 23:30:57 +0200

    Implement clear command

---
+++ src/kernel/commands/clear/clear.c
@@ -0,0 +1,34 @@
+#include "../../shell/command.h"
+
+#include "../../../common/stdio.h"
+#include "../../../drivers/video/vga/vga.h"
+
+/**
+ * main function of the clear command
+ * @param argc number of arguments
+ * @param argv array of argument strings
+ * @return return code of the command
+ */
+static int clear_execute(int argc, char **argv)
+{
+    (void)argv;
+
+    if (argc != 1)
+    {
+        printf("Usage: clear\r\n");
+        return -1;
+    }
+
+    vga_clear();
+
+    return 0;
+}
+
+static command_t clear_command =
+    {
+        .name = "clear",
+        .description = "Clear the terminal screen",
+        .usage = "clear",
+        .main = clear_execute};
+
+COMMAND_REGISTER(clear_command);
